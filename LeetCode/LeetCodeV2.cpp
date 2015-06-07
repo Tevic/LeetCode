0c2e5a4ff97742f393c38ebec837d9ad9e611dbd
Tevic <tevic.tt@gmail.com>
2015-06-07 17:17:20 +0800

    Update To Solution132

--- LeetCode/LeetCodeV2.cpp
+++ LeetCode/LeetCodeV2.cpp
@@ -2,7 +2,26 @@
 #include "DataStructure.h"
 
 
-
+/*-------------------------------------------------------------------------------------*/
+class Solution132 {
+public:
+	int minCut(string s) {
+		int len = s.size();
+		vector<int> D(len + 1);
+		vector<vector<bool>> P(len,vector<bool>(len,false));
+		for (int i = 0; i <= len; i++)
+			D[i] = len - i;
+		for (int i = len - 1; i >= 0; i--){
+			for (int j = i; j < len; j++){
+				if (s[i] == s[j] && (j - i<2 || P[i + 1][j - 1])){
+					P[i][j] = true;
+					D[i] = min(D[i], D[j + 1] + 1);
+				}
+			}
+		}
+		return D[0] - 1;
+	}
+};
 /*-------------------------------------------------------------------------------------*/
 class Solution131 {
 public:
