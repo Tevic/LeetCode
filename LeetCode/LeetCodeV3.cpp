c3f8559b07192cec3e4917166acc848cf04b8134
Tevic <tevic.tt@gmail.com>
2015-07-16 12:39:46 +0800

    UT238

--- LeetCode/LeetCodeV3.cpp
+++ LeetCode/LeetCodeV3.cpp
@@ -15,7 +15,28 @@
 
 
 
-
+/*-------------------------------------------------------------------------------------*/
+class Solution238 {
+public:
+	vector<int> productExceptSelf(vector<int>& nums) {
+		int len = nums.size();
+		vector<int> res(len,0);
+		int left = 1;
+		for (int i = 0; i < len; i++)
+		{
+			res[i] = left;
+			left *= nums[i];
+		}
+		int right = 1;
+		for (int i = len-1; i >=0; i--)
+		{
+			res[i] *= right;
+			right *= nums[i];
+		}
+		return res;
+	}
+};
+/*-------------------------------------------------------------------------------------*/
 class Solution237 {
 public:
 	void deleteNode(ListNode* node) {
