b566d50a57d73ea12546cbc96100a07f4704191d
Tevic <tevic.tt@gmail.com>
2015-09-27 16:11:08 +0800

    UT 284

--- LeetCode/LeetCodeV3.cpp
+++ LeetCode/LeetCodeV3.cpp
@@ -6,8 +6,87 @@
 
 
 
+/*-------------------------------------------------------------------------------------*/
+// Below is the interface for Iterator, which is already defined for you.
+// **DO NOT** modify the interface for Iterator.
+class Iterator {
+	struct Data;
+	Data* data;
+public:
+	Iterator(const vector<int>& nums);
+	Iterator(const Iterator& iter);
+	virtual ~Iterator();
+	// Returns the next element in the iteration.
+	int next();
+	// Returns true if the iteration has more elements.
+	bool hasNext() const;
+};
 
 
+class PeekingIterator : public Iterator {
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+		// Initialize any member here.
+		// **DO NOT** save a copy of nums and manipulate it directly.
+		// You should only use the Iterator interface methods.
+		if (Iterator::hasNext())
+		{
+			cur = Iterator::next();
+			mHasNext = true;
+		}
+
+	}
+
+	// Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+		return cur;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+		int pre = cur;
+		if (Iterator::hasNext())
+		{
+			cur = Iterator::next();
+		}
+		else
+		{
+			mHasNext = false;
+		}
+		return pre;
+	}
+
+	bool hasNext() const {
+		return mHasNext;
+	}
+private:
+	int cur;
+	bool mHasNext;
+};
+/*-------------------------------------------------------------------------------------*/
+class Solution283 {
+public:
+	void moveZeroes(vector<int>& nums) {
+		int len = nums.size();
+		int iCnt = 0;
+		for (size_t i = 0; i < len; i++)
+		{
+			if (nums[i]==0)
+			{
+				iCnt++;
+			}
+			else
+			{
+				nums[i - iCnt] = nums[i];
+				if (iCnt)
+				{
+					nums[i] = 0;
+				}
+			}
+		}
+	}
+};
 /*-------------------------------------------------------------------------------------*/
 class Solution279 {
 public:
